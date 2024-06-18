c03ca66c2ff9aa4ad3f25f19cb32447b0a3db0c8
hayat hani-tikass <hayathani-tikass@hayats-MacBook-Air.local>
2024-06-18 15:43:57 +0000

    added screens

---
+++ Screens/LogInScreen.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function LogInScreen() {
+  return (
+    <View>
+      <View>
+        <Text>Jobizz</Text>
+      </View>
+      <View>hjj</View>
+    </View>
+  )
+}
