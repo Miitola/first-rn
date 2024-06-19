8449287df334a7b4f0edcd19ac95b7ac3933c82b
hayat hani-tikass <hayathani-tikass@hayats-MacBook-Air.local>
2024-06-19 07:14:04 +0000

    images added

---
+++ mock/Cards.js
@@ -0,0 +1,78 @@
+import Facebook from "../assets/icons8-facebook-500.png";
+import Google from "../assets/icons8-google-480.png";
+import Microsoft from "../assets/icons8-microsoft-480.png";
+import Apple from "../assets/icons8-apple-500.png";
+import Amazon from "../assets/icons8-amazon-480.png";
+
+export const Cards = [
+  {
+    id: 0,
+    description:"Salary",
+    title: "Software Engineer",
+    text: "Facebook",
+    salary: 180000,
+    image: Facebook,
+    location: "Accra, Ghana",
+    color: "#4867AA",
+  },
+  {
+    id: 1,
+    description:"Salary",
+    title: "Software Engineer",
+    text: "Google",
+    salary: 160000,
+    image: Google,
+    location: "New York",
+    color: "#04284A",
+  },
+  {
+    id: 2,
+    description:"Salary",
+    title: "Dev Ops",
+    text: "Amazon",
+    salary: 200000,
+    location: "London",
+    color: "#232F3E",
+    image: Amazon,
+  },
+  {
+    id: 3,
+    description:"Salary",
+    title: "System Architect",
+    text: "Apple",
+    salary: 250000,
+    location: "London",
+    color: "black",
+    image: Apple,
+  },
+  {
+    id: 4,
+    description:"Salary",
+    title: "Developer",
+    text: "Microsoft",
+    salary: 180000,
+    location: "Texas",
+    color: "#19A6F0",
+    image: Microsoft,
+  },
+  {
+    id: 3,
+    description:"Salary",
+    title: "System Architect",
+    text: "Apple",
+    salary: 250000,
+    location: "London",
+    color: "black",
+    image: Apple,
+  },
+  {
+    id: 3,
+    description:"Salary",
+    title: "System Architect",
+    text: "Apple",
+    salary: 250000,
+    location: "London",
+    color: "black",
+    image: Apple,
+  },
+];
